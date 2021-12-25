d315e3378c3e4ebba1dcf924a045ce3644810964
hiruy-fasil <valjalyon@gmail.com>
2021-12-26 02:35:30 +0800

    updated

---
+++ script.js
@@ -0,0 +1,71 @@
+
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+
+formBtn.addEventListener('click', () =>{
+  loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+  loginForm.classList.remove('active');
+});
+
+
+
+var swiper = new Swiper(".review-slider", {
+  spaceBetween: 20,
+  loop:true,
+  autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+  },
+  breakpoints: {
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+  },
+});
+
+
+function upload_file(e) {
+  e.preventDefault();
+  ajax_file_upload(e.dataTransfer.files);
+}
+
+function file_explorer() {
+  document.getElementById('selectfile').click();
+  document.getElementById('selectfile').onchange = function() {
+      files = document.getElementById('selectfile').files;
+      ajax_file_upload(files);
+  };
+}
+
+function ajax_file_upload(files_obj) {
+  if(files_obj != undefined) {
+      var form_data = new FormData();
+      for(i=0; i<files_obj.length; i++) {
+          form_data.append('file[]', files_obj[i]);
+      }
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("POST", "ajax.php", true);
+      xhttp.onload = function(event) {
+          if (xhttp.status == 200) {
+              alert(this.responseText);
+          } else {
+              alert("Error " + xhttp.status + " occurred when trying to upload your file.");
+          }
+      }
+
+      xhttp.send(form_data);
+  }
+}
+
