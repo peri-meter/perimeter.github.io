fdcc9c9ed992093a8d1c10b04c1a4eb701ba71cd
hiruy-fasil <valjalyon@gmail.com>
2022-01-29 16:07:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
-/*
+
 window.onscroll = () =>{
   loginForm.classList.remove('active');
 }
-*/
+
 
 formBtn.addEventListener('click', () =>{
   loginForm.classList.add('active');
